07d4d49d3f2d009981422ec61e32739523c01737
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2026-02-04 19:46:05 +0100

    delete, edit icons and checkbox add

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,5 @@
 import React,{ useState } from "react"
+import "../index.css"
 
 
 const Content = () => {
@@ -12,10 +13,16 @@ const Content = () => {
     <ul className="list-group">
     {
         items.map((item) => {
-            return <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">{item.label}
+            return <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+            <div className="form-check">
+  <input className="form-check-input" type="checkbox" value="" id={`checkDefault${item.id}`} checked={item.checked}/>
+  <label className="form-check-label" htmlFor={`checkDefault${item.id}`}>
+    {item.label}
+  </label>
+</div>
             <span className="d-flex align-items-center gap-2">
-            <i className="bi bi-pencil-square">Edit</i>
-            <i className="bi bi-trash3 me-2">Delete</i>
+            <i className="bi bi-pencil-square icon-blue" tabIndex={0}>Edit</i>
+            <i className="bi bi-trash3 me-2 icon-red" tabIndex={0}>Delete</i>
             </span>
             
             
